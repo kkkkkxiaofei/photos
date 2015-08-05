b303174cb5b3bb9814602faa08fca385b105efa3
kkkkkxiaofei <brztzxf@gmail.com>
2015-08-05 20:33:30 +0800

    add a script to create xml for photo folder

---
+++ _site/script/1.js
@@ -0,0 +1,54 @@
+var fs = require('fs'), 
+    obj = {
+        'us-photo': [],
+        'family-photo': [],
+        'life-photo': [],
+        'favorite-photo': []
+    },
+    js2xmlparser = require("js2xmlparser");
+
+vistAllFiles(obj);
+function vistAllFiles(obj) {
+    _storeDir('../img/us-photo', obj['us-photo'], _getLen('../img/life-photo'), function() {
+        _storeDir('../img/family-photo', obj['family-photo'], _getLen('../img/family-photo'), function() {
+            _storeDir('../img/life-photo', obj['life-photo'], _getLen('../img/life-photo'), function() {
+                _storeDir('../img/favorite-photo', obj['favorite-photo'], _getLen('../img/favorite-photo'), function() {
+                    console.log(js2xmlparser('photos', obj));
+                });
+            });
+        });
+    });
+}
+
+function _getLen(dir) {
+    var files = fs.readdirSync(dir);
+    return files.length;
+}
+
+function _storeDir(dir, arr, count, callback) {
+    fs.readdir(dir, function(err, items) {
+        for (var i=0; i < items.length; i++) {
+            var file = dir + '/' + items[i];
+            fs.stat(file, (function(file, arr, index, callback) {
+                return function(err, stats) {
+                    arr.push({'file_name': file,'creted_at': stats.birthtime});
+                    if(index + 1 == count) {
+                        callback();
+                    }
+                }
+            })(file, arr, i, callback));
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
