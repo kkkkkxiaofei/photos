598d580b525775d84bd9391dcbfeb99c5f3d706f
kkkkkxiaofei <brztzxf@gmail.com>
2015-08-06 13:39:04 +0800

    modify the script using sync

--- script/1.js
+++ script/1.js
@@ -1,51 +1,8 @@
 var fs = require('fs'), 
-    obj = {
-        'us-photo': [],
-        'family-photo': [],
-        'life-photo': [],
-        'favorite-photo': []
-    },
+    obj = {},
     js2xmlparser = require("js2xmlparser");
 
-vistAllFiles(obj);
-function vistAllFiles(obj) {
-    _storeDir('../img/us-photo', obj['us-photo'], _getLen('../img/life-photo'), function() {
-        _storeDir('../img/family-photo', obj['family-photo'], _getLen('../img/family-photo'), function() {
-            _storeDir('../img/life-photo', obj['life-photo'], _getLen('../img/life-photo'), function() {
-                _storeDir('../img/favorite-photo', obj['favorite-photo'], _getLen('../img/favorite-photo'), function() {
-                    _wirteToXML(obj);
-                });
-            });
-        });
-    });
-}
-
-function _getLen(dir) {
-    var files = fs.readdirSync(dir);
-    return files.length;
-}
-
-function _storeDir(dir, arr, count, callback) {
-    fs.readdir(dir, function(err, items) {
-        for (var i=0; i < items.length; i++) {
-            var file = dir + '/' + items[i];
-            fs.stat(file, (function(file, arr, index, callback) {
-                return function(err, stats) {
-                    if(file.indexOf('DS_Store') == -1) {
-                        var date = new Date(stats.birthtime);
-                        var src = file.substr(3);
-                        arr.push({'file_name': src,'created_at': date.toLocaleDateString()});
-                    }
-                    if(index + 1 == count) {
-                        callback();
-                    }
-                }
-            })(file, arr, i, callback));
-        }
-    });
-}
-
-function _wirteToXML(obj) {
+function wirteToXML(obj) {
     var keys = Object.keys(obj);
     for(var i in keys) {
         obj[keys[i]].sort(function (a, b) {
@@ -58,10 +15,31 @@ function _wirteToXML(obj) {
     });
 }
 
+function readDir(dir, obj) {
+    var stats = fs.statSync(dir);
+    if(stats.isDirectory()) {
+        var files = fs.readdirSync(dir);
+        var key = dir.split('/').pop();
+        obj[key] = obj[key] || [];
+        for(var i in files) {
+            var path = dir + '/' + files[i];
+            readDir(path, obj);
+        }
+    } else {
+        var stat = fs.statSync(dir);
+        var arr = dir.split('/');
+        arr.pop();
+        var key = arr.pop();
+        if(dir.indexOf('DS_Store') == -1) {
+            var date = new Date(stat.birthtime);
+            var src = dir.substr(3);
+            obj[key].push({'file_name': src,'created_at': date.toLocaleDateString()});
+        }
+    }
+    return obj;
+}
 
-
-
-
+wirteToXML(readDir('../img', obj));
 
 
 
